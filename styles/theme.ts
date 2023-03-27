fea5e85b88594f416affde5b68677bd8d0d75c22
Ruan Campelo Neri <ruan.campelo@g.globo>
2023-03-27 18:06:04 -0300

    ajuste no estilo de cor.

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,7 +3,10 @@ import { extendBaseTheme } from '@chakra-ui/react'
 export const theme = extendBaseTheme ({
   styles : {
     global: {
-      bg: "red"
+      body : {
+        bg: "gray.900",
+        color: "gray.400",
+      }
     }
   }
 })
