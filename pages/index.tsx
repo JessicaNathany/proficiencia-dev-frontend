b3fb59411e89b2d0dd624a30d89b02af020c78cf
Ruan Campelo Neri <ruan.campelo@g.globo>
2023-03-27 18:37:02 -0300

    ajustes de layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { Flex } from "@chakra-ui/react";
 
 
 export default function Home() {
   return (
-   <h1>Proficiencia Dev</h1>
+   <Flex align="center" width="100vw" height="100vh" justify="center">
+    <h1>Proficiencia Dev</h1>
+   </Flex>
   )
 }
