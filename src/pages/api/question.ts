218cff84aed0029d780697c33d9aa7160ec3b181
Felipe Rosas <felipe.rosas@g.globo>
2023-03-31 15:42:28 -0300

    feat: update response question

--- src/pages/api/question.ts
+++ src/pages/api/question.ts
@@ -1,3 +1,4 @@
+import { OpenAIService } from "@/src/services";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import languageOptions from "../../data/languages";
@@ -21,19 +22,17 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     skillLevel,
   } = request.body;
 
-  const userSkillObj = skillOptions.filter((item) => item.value === userSkill);
-  const userLevelObj = levelOptions.filter((item) => item.value === userLevel);
-  const userLanguageObj = languageOptions.filter(
+  const userSkillObj = skillOptions.find((item) => item.value === userSkill);
+  const userLevelObj = levelOptions.find((item) => item.value === userLevel);
+  const userLanguageObj = languageOptions.find(
     (item) => item.value === userEnglishLevel
   );
 
-  const skillLanguageObj = programmingLanguageOptions.filter(
+  const skillLanguageObj = programmingLanguageOptions.find(
     (item) => item.value === skillLanguage
   );
 
-  const skillLevelObj = levelOptions.filter(
-    (item) => item.value === skillLevel
-  );
+  const skillLevelObj = levelOptions.find((item) => item.value === skillLevel);
 
   const data = {
     user: {
@@ -47,5 +46,72 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     skillLevel: skillLevelObj,
   };
 
-  return response.status(200).json(data);
+  const resultFormat = JSON.stringify([
+    {
+      id: 1,
+      question: "Qual a diferença entre uma função e um método?",
+      answers: [
+        {
+          id: 1,
+          answer:
+            "Uma função é um bloco de código que pode ser executado várias vezes, enquanto um método é um bloco de código que pode ser executado várias vezes, mas pertence a um objeto.",
+          correct: false,
+        },
+      ],
+    },
+  ]);
+
+  const prompt = `
+  Baseado em um usuário ${userSkillObj?.label} de nível ${userLevelObj?.label} 
+  e que fala Inglês ${userLanguageObj?.label}, 
+  sugira um questionário de múltipla escolha para ajudar a se preparar para uma prova de ${skillLanguageObj?.label},
+  retorne um questionário de múltipla escolha com 5 perguntas e 4 respostas cada, 
+  retorne tudo em json sem formatação no padrão : ${resultFormat}  `.trim();
+
+  try {
+    const result = await OpenAIService.completion(prompt);
+
+    const [resultData] = result;
+
+    if (!resultData) {
+      return response
+        .status(400)
+        .json({ error: "Nenhum resultado encontrado" });
+    }
+
+    const text = String(resultData.text);
+
+    const cleanResponse = text
+      .trim()
+      .replaceAll(/^\ufeff/g, "")
+      .replaceAll("\r", "")
+      .replaceAll("\t", "")
+      .replaceAll("\f", "")
+      .replaceAll("\n", "");
+
+    try {
+      const messageWithoutFirstCharacter = cleanResponse.replace(
+        /^([^{\[]*)/,
+        ""
+      );
+
+      const message = JSON.parse(messageWithoutFirstCharacter);
+
+      return response.status(200).json({
+        ...data,
+        prompt,
+        parsed: true,
+        message,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    return response
+      .status(200)
+      .json({ ...data, prompt, parsed: false, message: cleanResponse });
+  } catch (error) {
+    console.log(error);
+    return response.status(400).json({ error: "Error on OpenAI" });
+  }
 };
