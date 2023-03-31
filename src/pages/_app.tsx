5709fe0f7d30d4a5c1f80ac87bb8ab9c8c5038c3
Felipe Rosas <felipe.rosas@g.globo>
2023-03-30 21:19:49 -0300

    chore: update structure folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { theme } from "@/styles/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
